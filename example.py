64cbf6514c6d5dc42f495a885152479667516368
KorOnechild <kyuhong99@gmail.com>
2024-09-20 02:58:55 +0900

    update: discord.Embed 적용 디스코드 봇 메시지 디자인 변경

--- example.py
+++ example.py
@@ -16,17 +16,18 @@ titles = []  # 제목 큐
 is_playing = False  # 현재 노래 재생 상태 추적
 current_title = None  # 현재 재생 중인 노래의 제목 저장
 current_song = None  # 현재 재생 중인 노래의 URL 저장
+current_thumbnail = None # 현재 재생 중인 노래의 썸네일 url 저장
 ydl_opts = {
     'format': 'bestaudio/best',
     'quiet': True,
     'noplaylist': True,
     'extract_flat': False,  # 스트리밍만 하기 위해 설정
-    # 'cookiefile': '/home/ubuntu/app/cookies.txt'  # 쿠키 파일 경로
+    'cookiefile': '/home/ubuntu/app/cookies.txt'  # 쿠키 파일 경로
 }
 
 FFMPEG_OPTIONS = {
-    # 'executable': '/usr/bin/ffmpeg',
-    'executable': 'D:\\ffmpeg\\bin\\ffmpeg.exe',
+    'executable': '/usr/bin/ffmpeg',
+    # 'executable': 'D:\\ffmpeg\\bin\\ffmpeg.exe',
     'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
     'options': '-vn'
 }
@@ -41,7 +42,7 @@ def search_song(query):
 
 async def play_next_song(ctx):
     """큐에서 다음 노래를 재생."""
-    global is_playing, current_title, current_song
+    global is_playing, current_title, current_song, current_thumbnail
     if queue:
         is_playing = True
         current_song = queue.pop(0)
@@ -51,10 +52,15 @@ async def play_next_song(ctx):
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                 info = ydl.extract_info(current_song, download=False)
                 URL = info['url']
-
+                current_thumbnail = info['thumbnail']
             voice = bot.voice_clients[0]
             voice.play(discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS), after=lambda e: bot.loop.create_task(handle_after_play(ctx, e)))
-            await ctx.send(f"> \n> 노래를 재생합니다. : {current_song}\n> ")
+
+            embed = discord.Embed(colour=discord.Colour.red(), title='노래를 재생합니다.', description="노래 정보")
+            embed.set_image(url=current_thumbnail)
+            embed.add_field(name=current_title, value=current_song, inline=False)
+            await ctx.send(embed=embed)
+
         except Exception as e:
             is_playing = False
             await ctx.send(f"> \n> 노래 재생 중 오류가 발생했습니다. 다음 곡으로 넘어갑니다.\n> ")
@@ -71,6 +77,8 @@ async def handle_after_play(ctx, error):
         discord_send_message(str(error))
     await play_next_song(ctx)
 
+
+# /재생
 @bot.command(name='재생')
 async def play(ctx, *, search: str = None):
     """노래를 검색하고 재생 큐에 추가."""
@@ -87,7 +95,7 @@ async def play(ctx, *, search: str = None):
     try:
         if not bot.voice_clients:
             await ctx.author.voice.channel.connect()
-            await ctx.send(help())
+            await ctx.send(embed=help())
 
         # 노래 검색
         video_url = search_song(search)
@@ -99,10 +107,13 @@ async def play(ctx, *, search: str = None):
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(video_url, download=False)
             current_title = info['fulltitle']
+            current_thumbnail = info['thumbnail']
             queue.append(video_url)
             titles.append(current_title)
-
-        await ctx.send(f"> 재생 목록에 추가: {video_url}")
+        embed = discord.Embed(colour=discord.Colour.red(), title='노래를 재생목록에 추가합니다.', description='노래 정보')
+        embed.add_field(name=current_title, value=f"{video_url}", inline=False)
+        embed.set_thumbnail(url=current_thumbnail)
+        await ctx.send(embed=embed)
 
         # 재생 중이 아니면 바로 재생 시작
         if not is_playing:
@@ -110,40 +121,55 @@ async def play(ctx, *, search: str = None):
     except Exception as e:
         discord_send_message(e)
 
+
+# /넘기기
 @bot.command(name='넘기기')
 async def skip(ctx):
-    """현재 노래를 스킵하고 다음 노래를 재생."""
+    """현재 재생중인 노래를 스킵"""
+    embed=discord.Embed(colour=discord.Colour.red())
     try:
         if bot.voice_clients and bot.voice_clients[0].is_playing():
             bot.voice_clients[0].stop()
-            await ctx.send("> 현재 노래를 스킵합니다.")
+            embed.title="재생중인 노래를 스킵합니다..."
+            await ctx.send(embed=embed)
             await play_next_song(ctx)
         else:
-            await ctx.send("> 현재 재생 중인 노래가 없습니다.")
+            embed.title="현재 재생 중인 노래가 없습니다."
+            await ctx.send(embed=embed)
     except Exception as e:
         discord_send_message(e)
 
+
+# /재생목록
 @bot.command(name='재생목록')
 async def playlist(ctx):
     """현재 재생 목록을 출력."""
-    global current_title
+    global current_title, current_song
     try:
+        embed=discord.Embed(colour=discord.Colour.red())
         if current_title or titles:
-            playlist = ""
+            embed.title="재생목록"
             if current_title:
-                playlist += f"> 현재 재생 중: {current_title}\n"
+                embed.add_field(name=current_title, value=current_song, inline=False)
+                embed.set_thumbnail(url=current_thumbnail)
             if titles:
-                playlist += "\n > ".join([f"{i+1}. {title}" for i, title in enumerate(titles)])
-            await ctx.send(f"{playlist}")
+                embed.add_field(name="현재 재생목록입니다.", value="")
+                for i, title in enumerate(titles):
+                    embed.add_field(name=f"{i+1}. {title}", value=queue[i], inline=False)
+            await ctx.send(embed=embed)
         else:
-            await ctx.send("> 재생 목록이 비어 있습니다.")
+            embed.title="재생목록이 비어있습니다."
+            await ctx.send(embed=embed)
     except Exception as e:
         discord_send_message(e)
 
+
+#종료
 @bot.command(name='종료')
 async def leave(ctx):
     """봇이 음성 채널에서 나가고 재생 목록을 초기화."""
     global is_playing, current_title, current_song
+    embed=discord.Embed(colour=discord.Colour.red())
     try:
         if bot.voice_clients:
             await bot.voice_clients[0].disconnect()
@@ -153,14 +179,16 @@ async def leave(ctx):
             current_song = None
             current_title = None
         else:
-            await ctx.send("> 봇이 음성 채널에 연결되어 있지 않습니다.")
+            embed.title="봇이 음성 채널에 연결되어 있지 않습니다."
+            await ctx.send(embed=embed)
     except Exception as e:
         discord_send_message(e)
 
+#도움말
 @bot.command(name='도움말')
 async def doc(ctx):
     """봇이 음성 채널에서 나가고 재생 목록을 초기화."""
-    await ctx.send(help())
+    await ctx.send(embed=help())
    
 
 def discord_send_message(text):
@@ -169,18 +197,14 @@ def discord_send_message(text):
     requests.post(discord_webhook_url, data=message)
 
 def help():
-    help = """
-
-> ## 납치된 노래봇 사용법
-> 
-> - `/재생 { 제목 or URL }` :   노래 재생 (제목 입력 시 { } 는 빼고 입력)
-> 
-> - `/재생목록` :    재생목록 확인
-> 
-> - `/종료`:    종료   
-> 
-> - `/도움말` :  도움말
-"""
-    return help
+    embed = discord.Embed(
+    title="납치된 노래봇 사용법",
+    color=discord.Color.red()  # 원하는 색상으로 바꿀 수 있음
+    )
+    embed.add_field(name="` /재생 { 제목 or URL } `", value="노래 재생(제목 입력 시 { }는 빼고 입력)", inline=False)
+    embed.add_field(name="` /재생목록 `", value="현재 재생목록을 보여줍니다.", inline=False)
+    embed.add_field(name="` /종료 `", value="노래봇 종료", inline=False)
+    embed.add_field(name="` /도움말 `", value="도움말을 보여줍니다.", inline=False)
+    return embed
 
 bot.run('MTI4NTYzMzI2OTgyNzg5NTMzNw.GY5_IJ.ss7v3FVZJfnzCJbFEqDeUWMpaQXas9XRqfRYOc')
