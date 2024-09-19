40611a0cb5bf2f078cd8f3fb70d325c9fcaf769c
KorOnechild <kyuhong99@gmail.com>
2024-09-20 00:56:49 +0900

    update: md 작업

--- example.py
+++ example.py
@@ -54,10 +54,10 @@ async def play_next_song(ctx):
 
             voice = bot.voice_clients[0]
             voice.play(discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS), after=lambda e: bot.loop.create_task(handle_after_play(ctx, e)))
-            await ctx.send(f"현재 재생 중: {current_title}")
+            await ctx.send(f"> \n> 노래를 재생합니다. : {current_song}\n> ")
         except Exception as e:
             is_playing = False
-            await ctx.send(f"노래 재생 중 오류가 발생했습니다. 다음 곡으로 넘어갑니다.")
+            await ctx.send(f"> \n> 노래 재생 중 오류가 발생했습니다. 다음 곡으로 넘어갑니다.\n> ")
             discord_send_message(e)
             await play_next_song(ctx)  # 오류 발생 시에도 다음 노래 재생
     else:
@@ -67,7 +67,7 @@ async def play_next_song(ctx):
 async def handle_after_play(ctx, error):
     """노래 재생 후 발생하는 오류 처리."""
     if error:
-        await ctx.send(f"오류 발생: {str(error)}. 다음 곡으로 넘어갑니다.")
+        await ctx.send(f"> 오류 발생: {str(error)}. 다음 곡으로 넘어갑니다.")
         discord_send_message(str(error))
     await play_next_song(ctx)
 
@@ -77,23 +77,22 @@ async def play(ctx, *, search: str = None):
     global is_playing
 
     if not search:
-        await ctx.send("검색어를 입력해주세요.")
+        await ctx.send("> 검색어를 입력해주세요.")
         return
 
     if not ctx.author.voice:
-        await ctx.send("먼저 음성 채널에 들어가야 합니다.")
+        await ctx.send("> 먼저 음성 채널에 들어가야 합니다.")
         return
 
     try:
         if not bot.voice_clients:
             await ctx.author.voice.channel.connect()
-            await ctx.send("음성 채널에 연결되었습니다.")
             await ctx.send(help())
 
         # 노래 검색
         video_url = search_song(search)
         if not video_url:
-            await ctx.send(f"'{search}'에 대한 검색 결과가 없습니다.")
+            await ctx.send(f"> '{search}'에 대한 검색 결과가 없습니다.")
             return
 
         # 검색된 노래 재생 목록에 추가
@@ -103,7 +102,7 @@ async def play(ctx, *, search: str = None):
             queue.append(video_url)
             titles.append(current_title)
 
-        await ctx.send(f"재생 목록에 추가: {current_title}")
+        await ctx.send(f"> 재생 목록에 추가: {video_url}")
 
         # 재생 중이 아니면 바로 재생 시작
         if not is_playing:
@@ -117,10 +116,10 @@ async def skip(ctx):
     try:
         if bot.voice_clients and bot.voice_clients[0].is_playing():
             bot.voice_clients[0].stop()
-            await ctx.send("현재 노래를 스킵합니다.")
+            await ctx.send("> 현재 노래를 스킵합니다.")
             await play_next_song(ctx)
         else:
-            await ctx.send("현재 재생 중인 노래가 없습니다.")
+            await ctx.send("> 현재 재생 중인 노래가 없습니다.")
     except Exception as e:
         discord_send_message(e)
 
@@ -132,12 +131,12 @@ async def playlist(ctx):
         if current_title or titles:
             playlist = ""
             if current_title:
-                playlist += f"현재 재생 중: {current_title}\n"
+                playlist += f"> 현재 재생 중: {current_title}\n"
             if titles:
-                playlist += "\n".join([f"{i+1}. {title}" for i, title in enumerate(titles)])
+                playlist += "\n > ".join([f"{i+1}. {title}" for i, title in enumerate(titles)])
             await ctx.send(f"{playlist}")
         else:
-            await ctx.send("재생 목록이 비어 있습니다.")
+            await ctx.send("> 재생 목록이 비어 있습니다.")
     except Exception as e:
         discord_send_message(e)
 
@@ -153,9 +152,8 @@ async def leave(ctx):
             is_playing = False
             current_song = None
             current_title = None
-            await ctx.send("봇이 음성 채널을 떠났습니다.")
         else:
-            await ctx.send("봇이 음성 채널에 연결되어 있지 않습니다.")
+            await ctx.send("> 봇이 음성 채널에 연결되어 있지 않습니다.")
     except Exception as e:
         discord_send_message(e)
 
@@ -172,18 +170,17 @@ def discord_send_message(text):
 
 def help():
     help = """
--------------------------------------------------------------
-## 납치된 노래봇 사용법 
 
-`/재생 { 제목 }`: 노래 재생 (제목 입력 시 { } 는 빼고 입력)
-
-`/재생목록`: 재생목록 확인                                   
-
-`/종료`: 종료
-
-`/도움말`: 도움말
--------------------------------------------------------------
-    """
+> ## 납치된 노래봇 사용법
+> 
+> - `/재생 { 제목 or URL }` :   노래 재생 (제목 입력 시 { } 는 빼고 입력)
+> 
+> - `/재생목록` :    재생목록 확인
+> 
+> - `/종료`:    종료   
+> 
+> - `/도움말` :  도움말
+"""
     return help
 
 bot.run('MTI4NTYzMzI2OTgyNzg5NTMzNw.GY5_IJ.ss7v3FVZJfnzCJbFEqDeUWMpaQXas9XRqfRYOc')
